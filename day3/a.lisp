713c6ae001b148bd6e01fb30027d9f92763ca16b
Justin <justin2004@hotmail.com>
2021-12-04 12:50:18 -0500

    day 3 p2

--- day3/a.lisp
+++ day3/a.lisp
@@ -17,4 +17,44 @@
 (april "gammaBin←half≤+⌿m")
 (april "(2⊥~gammaBin)×(2⊥gammaBin)")
 
+; part 2
+(april "keep←{alterBits←⍺  
+              iter←⊃⍵[1]
+              $[¯1=iter ;   ⍝ if iter is -1 then we just need to break
+               ¯1 ,(⊂gammaBin),↓⊃⍵[3] ;
+                ⍝gammaBin←alterBits ⊃⍵[2]
+                ⎕←'applying to'
+                ⎕←⊃⍵[2]
+                gammaBin←$[alterBits=1;~⊃⍵[2];⊃⍵[2]]
+                ⎕←'------------'
+                ⎕←'rowNum'
+                ⎕←rowNum←iter  ⍝ i think i mean colNum
+                ⎕←'what'
+                ⎕←what←iter⌷gammaBin
+                ⎕←'matFiltered'
+                ⎕←matFiltered←↑(∊what=rowNum⌷¨↓⊃⍵[3])/↓⊃⍵[3]
+                ⎕←'half'
+                ⎕←half←(numRows matFiltered)÷2
+                ⎕←'gammaBin first'
+                ⎕←gammaBin
+                ⎕←'gammaBin second'
+                ⎕←gammaBin←half≤+⌿matFiltered
+                ⍝⎕←gammaBin←half<+⌿matFiltered
+                $[1=⊃1↑⍴matFiltered; ⍝ if we have 1 row left
+                  ⎕←'1 row left'
+                 ¯1 ,(⊂gammaBin),⊂matFiltered ;  ⍝ return the args-ish
+                  (iter+1),(⊂gammaBin),⊂matFiltered   ⍝ else keep going
+               ]]
+                }")
+
+
+; (april "keep keep keep keep keep keep (keep 1 gammaBin m)")
+; (april-f "argPairs←↓gammaBin,[1.5]⍨⍳⍴gammaBin")
+; i think i should learn @
+
+(april "oxygenGeneratorRating←(2⊥⊃3⌷(1∘keep⍣≡ 1 gammaBin m))")
+(april "co2ScrubberRating←(2⊥⊃3⌷(0∘keep⍣≡ 1 gammaBin m))")
+(april "oxygenGeneratorRating×co2ScrubberRating")
+
+
 
